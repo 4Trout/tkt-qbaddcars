e32c1a2e051804240d817f445af3ca69579387ad
4Trout <56208177+4Trout@users.noreply.github.com>
2023-01-01 04:16:49 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+game 'gta5'
+
+name 'tkt-qbaddcars'
+description 'A simple utility for QBCore Admins to add cars'
+author 'Trout#1313'
+version 'v1.0.0'
+
+server_scripts {
+    'server.lua',
+}
